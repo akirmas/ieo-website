a9f3ecadf5d97f967de6fd506932e77b5f395bcd
Roman <rmnbrtshk@gmail.com>
2019-04-25 12:43:23 +0300

    Apply BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
@@ -8,7 +9,9 @@ import * as serviceWorker from './serviceWorker';
 import './styles/main.scss';
 
 const Index = () => (
-  <App />
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>
 );
 
 const RootElement = document.getElementById('root');
