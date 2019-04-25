4e94e7d8a3d4505e159f1340ef5c2ea45ed7cf1c
Roman <rmnbrtshk@gmail.com>
2019-04-25 14:01:27 +0300

    Apply ImageSlider component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 import Template from './components/layout/Template';
+import ImageSlider from './components/widgets/ImageSlider';
+
+const images = [
+  {
+    src: 'images/banners/third.png'
+  },   {
+    src: 'images/banners/second.png'
+  },   {
+    src: 'images/banners/first.png'
+  },
+];
 
 const App = () => (
   <main className="app">
-    <Template></Template>
+    <Template>
+      <ImageSlider {...{images}} />
+    </Template>
   </main>
 );
 
