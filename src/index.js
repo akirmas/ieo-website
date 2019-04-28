7e6b8ae2b6625fdfe3e3b8676671dc08133ac175
Bogdan <bogdan7morozyuk@gmail.com>
2019-04-28 16:05:16 +0300

    Apply ErrorBoundary component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import ErrorBoundary from './components/services/ErrorBoundary';
 
 import * as serviceWorker from './serviceWorker';
 
 import './styles/main.scss';
 
 const Index = () => (
-  <Router basename={process.env.PUBLIC_URL}>
-    <App />
-  </Router>
+  <ErrorBoundary>
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+  </ErrorBoundary>
 );
 
 const RootElement = document.getElementById('root');
