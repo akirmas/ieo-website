49cf528e37da6fd4cec6662dc92a21a29cbb7ce2
Roman <rmnbrtshk@gmail.com>
2019-04-29 17:18:58 +0300

    Update template: use data from context and apply Footer component

--- src/components/layout/Template/Template.js
+++ src/components/layout/Template/Template.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 import Header from '../Header';
+import Footer from '../Footer';
+
+import { AppContext } from '../../../context/app';
 
 import './Template.scss';
 
 const Template = ({ children, location }) => {
   const variant = location.pathname === '/' ? 'gray' : 'light';
 
+  const { template: {
+    footer
+  } } = useContext(AppContext);
+
   return (
     <>
       <Header {...{variant}} />
       {children}
+      <Footer {...{variant}} {...footer} />
     </>
   );
 };
