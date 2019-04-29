a331b86fec895b4375af6cc4074e34c46bd973ad
Roman <rmnbrtshk@gmail.com>
2019-04-29 12:45:16 +0300

    Load application data

--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import App from './App';
+import { AppProvider } from './context/app';
 import ErrorBoundary from './components/services/ErrorBoundary';
 
 import * as serviceWorker from './serviceWorker';
 
+import { resolvePath } from './helpers';
+
 import './styles/main.scss';
 
-const Index = () => (
+const Index = ({ data }) => (
   <ErrorBoundary>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
+    <AppProvider value={data}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <App />
+      </Router>
+    </AppProvider>
   </ErrorBoundary>
 );
 
+Index.propTypes = {
+  data: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object,
+  ]).isRequired,
+};
+
 const RootElement = document.getElementById('root');
 
-render(
-  <Index />,
-  RootElement 
-);
+const setup = (file) => {
+  fetch(resolvePath(file))
+    .then(response => response.json())
+    .then(data => {
+      render(
+        <Index {...{data}} />,
+        RootElement 
+      );
+    })
+    // eslint-disable-next-line no-console
+    .catch(console.error);
+};
+
+setup('data.json');
 
 serviceWorker.unregister();
