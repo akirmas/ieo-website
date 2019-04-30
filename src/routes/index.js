f27f2ef471dcfeed72e4d0dfa67c5e92336fcf4d
Roman <rmnbrtshk@gmail.com>
2019-04-30 10:50:17 +0300

    Register a new route: /about/key-roles

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,16 @@ import { lazy } from 'react';
 import { withProps } from '../hocs';
 
 const Home = lazy(() => import('../pages/Home'));
+const KeyRoles = lazy(() => import('../pages/KeyRoles'));
 
 const routes = [
   {
     path: '/',
     exact: true,
     render: withProps(Home),
+  }, {
+    path: '/about/key-roles',
+    render: withProps(KeyRoles),
   }
 ];
 
