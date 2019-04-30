c4bf1cbcc2ebd94b9acc97b46f8bea4cfbb56da9
Roman <rmnbrtshk@gmail.com>
2019-04-30 10:49:08 +0300

    Create component: Team

---
+++ src/components/sections/Team/Team.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Teammate from '../../blocks/Teammate';
+
+import './Team.scss';
+
+const Team = ({ team }) => {
+  const renderTeammate = (teammate) => (
+    <Teammate key={teammate.name} {...teammate} />
+  );
+
+  return (
+    <section className="team">
+      <div className="container container--wide">
+        <main className="team__main">
+          {team.map(renderTeammate)}
+        </main>
+      </div>
+    </section>
+  );
+};
+
+Team.propTypes = {
+  team: PropTypes.arrayOf(
+    PropTypes.shape({
+      photo: PropTypes.string.isRequired,
+      position: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      info: PropTypes.string.isRequired,
+    })
+  ).isRequired
+};
+
+export default Team;
