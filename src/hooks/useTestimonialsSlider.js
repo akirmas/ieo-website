a44449ece28c4df40dbc0d1f80639644bf4ebac3
Roman <rmnbrtshk@gmail.com>
2019-05-01 09:38:32 +0300

    Create custom hook: useTestimonialsSlider

---
+++ src/hooks/useTestimonialsSlider.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+const useSlider = (slides, delay = 5000, doSlide = true) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [goRight, setGoRight] = useState(true);
+  const length = slides.length;
+
+  useEffect(() => {
+    if (doSlide) {
+      const interval = setInterval(() => {
+        if (goRight) {
+          setCurrentSlide(currentSlide + 1);
+          currentSlide === length - 2 && setGoRight(!goRight);
+        } else {
+          setCurrentSlide(currentSlide - 1);
+          currentSlide === 1 && setGoRight(!goRight);
+        }
+      }, delay);
+
+      return () => clearInterval(interval);
+    }
+  }, [currentSlide, length, delay, doSlide, goRight]);
+
+  const nextSlide = () =>
+    currentSlide < length - 1 && setCurrentSlide(currentSlide + 1);
+
+  const prevSlide = () => currentSlide && setCurrentSlide(currentSlide - 1);
+
+  const setSlide = id => setCurrentSlide(id);
+
+  return {
+    currentSlide,
+    nextSlide,
+    prevSlide,
+    setSlide
+  };
+};
+
+export default useSlider;
