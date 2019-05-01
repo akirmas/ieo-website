ce1fe5ba4c68c74ba4f3f7eb5dbb4044bc00824c
Roman <rmnbrtshk@gmail.com>
2019-05-01 09:38:56 +0300

    Create component: Testimonials

---
+++ src/components/sections/Testimonials/Testimonials.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+import Slider from '../../blocks/TestimonialsSlider/Slider';
+import ContentBox from '../../blocks/TestimonialsContentBox/ContentBox';
+import useTestimonialsSlider from '../../../hooks/useTestimonialsSlider';
+
+import './Testimonials.scss';
+
+const Testimonials = ({ title, items }) => {
+  const { currentSlide, nextSlide, prevSlide, setSlide } = useTestimonialsSlider(items);
+
+  const ButtonControl = ({ go, faShape }) => (
+    <button className="testimonials__arrow-btn" onClick={go}>
+      <FontAwesomeIcon className="testimonials__arrow-svg" icon={faShape} />
+    </button>
+  );
+
+  return (
+    <section className="testimonials">
+      <div className="container">
+        <header className="testimonials__header">
+          <h3 className="testimonials__title">{title}</h3>
+        </header>
+        <main className="testimonials__main">
+          <div className="testimonials__slider-area">
+            <div className="testimonials__button-container">
+              <ButtonControl faShape={faAngleRight} go={prevSlide} />
+            </div>
+            <Slider {...{ items, currentSlide, setSlide }} />
+            <div className="testimonials__button-container">
+              <ButtonControl faShape={faAngleLeft} go={nextSlide} />
+            </div>
+          </div>
+          <div>           
+            <ContentBox {...items[currentSlide].comment} />
+          </div>
+        </main>
+      </div>
+    </section>
+  );
+};
+
+Testimonials.propTypes = {
+  title: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.object
+  ).isRequired,
+};
+
+export default Testimonials;
