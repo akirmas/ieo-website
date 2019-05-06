6a9c43ea3dd5db74087aed2d0bba8dc58dc34349
Roman <rmnbrtshk@gmail.com>
2019-05-06 12:12:24 +0300

    Update key-roles page: apply Showcase component

--- src/pages/KeyRoles.js
+++ src/pages/KeyRoles.js
@@ -3,17 +3,22 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/app';
 
 import Team from '../components/sections/Team';
+import Showcase from '../components/sections/Showcase';
 
 const KeyRoles = () => {
   const { pages: { 
     keyRoles: {
       team,
+      showcase: {
+        images
+      }
     } 
   } } = useContext(AppContext); 
 
   return (
     <>
       <Team {...{team}} />
+      <Showcase {...{images}} />
     </>
   );
 };
