e7495b360c81fc0b21425d5ecc76ad942331a86a
Roman <rmnbrtshk@gmail.com>
2019-05-08 14:46:59 +0300

    Fix helper function renderText and create the new one  renderContent

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -16,5 +16,51 @@ export const classnames = (...classList) =>
 
 export const renderText = (text) =>
   Array.isArray(text)
-    ? text.map(sentence => <Fragment key={sentence}>{sentence} <br /></Fragment>)
+    ? text.map((sentence, index) => (
+      <Fragment 
+        key={sentence}
+      >
+        {sentence} 
+        {index < text.length - 1  && <br />}
+      </Fragment>
+    ))
     : text;
+
+export const renderContent = (content, blockName, variant) => 
+  content.map(contentGroup => {
+    return Object.entries(contentGroup).map(([ key, value ]) => {
+      switch (key) {
+      case 'title': 
+        return (
+          <h3
+            key={value}
+            className={classnames(
+              `${blockName}__title`,
+              variant ? `${blockName}__text--theme-${variant}` : ''
+            )}
+          >
+            {value}
+          </h3>
+        );
+      case 'text': 
+        return (
+          <div 
+            className={`${blockName}__text-group`}
+          >
+            {Array.isArray(value)
+              ? value.map(text => (
+                <p 
+                  key={text}              
+                  className={`${blockName}__text`}
+                >
+                  {renderText(text)}
+                </p>
+              ))
+              : renderText(value)}
+          </div>
+        );
+      default: 
+        return '';
+      }
+    });
+  });
