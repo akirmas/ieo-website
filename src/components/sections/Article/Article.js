5ad81724ba298fb4c79638fd4b3b96339545eba1
Roman <rmnbrtshk@gmail.com>
2019-05-08 14:47:25 +0300

    Create component: Article

---
+++ src/components/sections/Article/Article.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Image from '../../shared/Image';
+import Button from '../../shared/Button';
+
+import { renderContent, classnames } from '../../../helpers';
+
+import './Article.scss';
+
+const Article = ({ variant, banner, content }) => (
+  <section className="article">
+    <div className="container container--normal">
+      <main className="article__main">
+        <Image className="article__banner" {...banner} />
+        {renderContent(content, 'article', variant)}
+        <Button 
+          className={classnames(
+            'article__contact-button',
+            variant ? `article__contact-button--theme-${variant}` : ''
+          )}
+        >
+          ליצירת קשר  
+        </Button>
+      </main>
+    </div>
+  </section>
+);
+
+Article.propTypes = {
+  variant: PropTypes.string,
+  banner: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string,
+  }).isRequired,
+  content: PropTypes.array.isRequired,
+};
+
+export default Article;
