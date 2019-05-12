43852573302dd28fc27de9146ec4f1b1b26e6987
Roman <rmnbrtshk@gmail.com>
2019-05-12 18:56:59 +0300

    Create service: ScrollToTop

---
+++ src/components/services/ScrollToTop.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+class ScrollToTop extends Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+ScrollToTop.propTypes = {
+  location: PropTypes.object.isRequired,
+  children: PropTypes.any,
+};
+
+export default withRouter(ScrollToTop);
