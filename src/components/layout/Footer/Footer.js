e3d296c464cf8504ba7713335c8c31ed7ba32f53
Roman <rmnbrtshk@gmail.com>
2019-05-12 10:34:58 +0300

    Add contacts

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -2,14 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import NavBottom from '../NavBottom';
+import SocialMedia from '../../widgets/SocialMedia';
 
 import './Footer.scss';
 
-const Footer = ({ variant = 'light', navigation, copyright }) => (
+const Footer = ({ variant = 'light', navigation, contacts, copyright }) => (
   <footer className={`footer footer--theme-${variant}`}>
     <div className="container container--normal">
       <main className="footer__main">
         <NavBottom config={navigation.sub} />
+        {variant === 'light' && (
+          <SocialMedia {...{contacts}} />
+        )}
         <p className="copyright">{copyright}</p>
       </main>
     </div>
@@ -23,5 +27,6 @@ Footer.propTypes = {
     'gray', 'light'
   ]),
   navigation: PropTypes.object.isRequired,
+  contacts: PropTypes.array,
   copyright: PropTypes.string.isRequired,
 };
