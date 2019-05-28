20d709b490391390a5a5c3e61322b744f880ab06
Bogdan <bogdan7morozyuk@gmail.com>
2019-05-28 20:31:42 +0300

    Fix warning: 'relative' is defined but never used

--- src/components/blocks/TestimonialsSlider/Slider.js
+++ src/components/blocks/TestimonialsSlider/Slider.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Slide from './Slide';
-import { relative } from 'path';
 
 const Slider = ({ items, currentSlide, setSlide }) => {
   const slideClassName = i =>
