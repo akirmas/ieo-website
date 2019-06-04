1f5c4abd610040e0f48d4047f8718a585fc4b0de
Roman <rmnbrtshk@gmail.com>
2019-06-04 10:30:29 +0300

    Add link to WhoWeAre component

--- src/components/sections/WhoWeAre/WhoWeAre.js
+++ src/components/sections/WhoWeAre/WhoWeAre.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './WhoWeAre.scss';
+import { Link } from "react-router-dom";
+
+import "./WhoWeAre.scss";
 
 const WhoWeAre = ({ title, text }) => (
   <section className="who-we-are">
     <main className="who-we-are__main">
       <h2 className="who-we-are__title">{title}</h2>
-      <p className="who-we-are__text">{text}</p>
+      <p className="who-we-are__text">
+        {text}{" "}
+        <Link to="./about/who-we-are">
+          <span className="who-we-are__link">...להמשך קריאה</span>
+        </Link>
+      </p>
     </main>
   </section>
 );
 
 WhoWeAre.propTypes = {
   title: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
 };
 
 export default WhoWeAre;
