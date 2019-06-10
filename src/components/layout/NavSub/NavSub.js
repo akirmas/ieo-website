b7443240dbe8c7d15b8da6a59fde2bee19559b4d
Roman <rmnbrtshk@gmail.com>
2019-06-10 17:40:53 +0300

    Change NavSub menus to be started from the right

--- src/components/layout/NavSub/NavSub.js
+++ src/components/layout/NavSub/NavSub.js
@@ -1,53 +1,88 @@
-import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Link, withRouter } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, withRouter } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faChevronLeft
+} from "@fortawesome/free-solid-svg-icons";
 
-import { AppContext } from '../../../context/app';
+import { AppContext } from "../../../context/app";
 
-import './NavSub.scss';
+import "./NavSub.scss";
 
 const NavSub = ({ location: { pathname } }) => {
-  const { template: {
-    navigation: {
-      sub
+  const {
+    template: {
+      navigation: { sub }
     }
-  } } = useContext(AppContext);
+  } = useContext(AppContext);
 
-  const links = Object
-    .values(sub)
-    .filter(array => array.find(object => object['to'] === pathname))[0]
+  const links = Object.values(sub)
+    .filter(array => array.find(object => object["to"] === pathname))[0]
     .slice()
     .reverse();
 
-  const activeLinkIndex = links.findIndex(link => link['to'] === pathname);
+  const activeLinkIndex = links.findIndex(link => link["to"] === pathname);
 
   const prev = activeLinkIndex > 0 ? links[activeLinkIndex - 1] : null;
+  const beforePrev = activeLinkIndex > 1 ? links[activeLinkIndex - 2] : null;
   const current = links[activeLinkIndex];
-  const next = activeLinkIndex < links.length - 1 ? links[activeLinkIndex + 1] : null;
+  const next =
+    activeLinkIndex < links.length - 1 ? links[activeLinkIndex + 1] : null;
+  const afterNext =
+    activeLinkIndex < links.length ? links[activeLinkIndex] : null;
 
   return (
     <nav className="nav-sub">
       <div className="container container--normal">
         <main className="nav-sub__main">
-          {next ? (
+          {afterNext ? (
             <>
-              <Link className="nav-sub__chevron nav-sub__chevron--right" to={next.to}>
-                <FontAwesomeIcon icon={faChevronRight} />
+              {next && (
+                <Link
+                  className="nav-sub__chevron nav-sub__chevron--right"
+                  to={next.to}
+                >
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </Link>
+              )}
+
+              <Link
+                className={`nav-sub__link nav-sub__link--active nav-sub__link--${
+                  current.variant
+                }`}
+                to={current.to}
+              >
+                {current.name}
               </Link>
-              <Link className="nav-sub__link nav-sub__link--roily" to={next.to}>{next.name}</Link>
             </>
-          ) : <div className="stub" />}
-          <Link className={`nav-sub__link nav-sub__link--active nav-sub__link--${current.variant}`} to={current.to}>{current.name}</Link>
+          ) : (
+            <div className="stub" />
+          )}
           {prev ? (
             <>
-              <Link className="nav-sub__link nav-sub__link--roily" to={prev.to}>{prev.name}</Link>
-              <Link className="nav-sub__chevron nav-sub__chevron--left" to={prev.to}>
+              <Link className="nav-sub__link nav-sub__link--roily" to={prev.to}>
+                {prev.name}
+              </Link>
+              {beforePrev && (
+                <Link
+                  className="nav-sub__link nav-sub__link--roily"
+                  to={prev.to}
+                >
+                  {prev.name}
+                </Link>
+              )}
+              <Link
+                className="nav-sub__chevron nav-sub__chevron--left"
+                to={prev.to}
+              >
                 <FontAwesomeIcon icon={faChevronLeft} />
               </Link>
             </>
-          ) : <div className="stub" />}
+          ) : (
+            <div className="stub" />
+          )}
         </main>
       </div>
     </nav>
@@ -55,7 +90,7 @@ const NavSub = ({ location: { pathname } }) => {
 };
 
 NavSub.propTypes = {
-  location: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired
 };
 
 export default withRouter(NavSub);
