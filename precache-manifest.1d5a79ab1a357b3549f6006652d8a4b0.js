e7e3f7353b540fb45335a9f919473f353eaded82
Yurii Lubynets <yurichix1@gmail.com>
2019-11-20 11:13:40 +0200

    Updates

---
+++ precache-manifest.1d5a79ab1a357b3549f6006652d8a4b0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64e75e32cfe111c3b563f16f9de4a7c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab1f374bf75550131911",
+    "url": "./static/css/3.53154b79.chunk.css"
+  },
+  {
+    "revision": "f6ceba4ef4d9f0e31446",
+    "url": "./static/css/4.a9464897.chunk.css"
+  },
+  {
+    "revision": "639933ae643724cdafa6",
+    "url": "./static/css/5.a04f43c1.chunk.css"
+  },
+  {
+    "revision": "bd0c98d6dd8aa0a3e174",
+    "url": "./static/css/6.4d483498.chunk.css"
+  },
+  {
+    "revision": "b245cdfe2e1b5588c86a",
+    "url": "./static/css/7.69451d5c.chunk.css"
+  },
+  {
+    "revision": "0d97d8e5fcf32bdd0620",
+    "url": "./static/css/main.7d5dbd9d.chunk.css"
+  },
+  {
+    "revision": "f1a2f15c4d0b61e2a34a",
+    "url": "./static/js/0.c5a8b2c7.chunk.js"
+  },
+  {
+    "revision": "ab1f374bf75550131911",
+    "url": "./static/js/3.e1ffbeae.chunk.js"
+  },
+  {
+    "revision": "f6ceba4ef4d9f0e31446",
+    "url": "./static/js/4.8a35138b.chunk.js"
+  },
+  {
+    "revision": "639933ae643724cdafa6",
+    "url": "./static/js/5.82976b69.chunk.js"
+  },
+  {
+    "revision": "bd0c98d6dd8aa0a3e174",
+    "url": "./static/js/6.52d97033.chunk.js"
+  },
+  {
+    "revision": "b245cdfe2e1b5588c86a",
+    "url": "./static/js/7.ea24aecd.chunk.js"
+  },
+  {
+    "revision": "23aa1cb647cfb9f69ae5",
+    "url": "./static/js/8.12e8408c.chunk.js"
+  },
+  {
+    "revision": "ae7226074c53d6417f83",
+    "url": "./static/js/9.0b2ce251.chunk.js"
+  },
+  {
+    "revision": "0d97d8e5fcf32bdd0620",
+    "url": "./static/js/main.bd51aab9.chunk.js"
+  },
+  {
+    "revision": "102a33bd4e39aec15536",
+    "url": "./static/js/runtime~main.f5413057.js"
+  }
+]);
